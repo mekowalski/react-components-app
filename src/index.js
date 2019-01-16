7b2a92cc16be8ddd840af08f6620eb191b226f87
Malind Kowalski <malind.nikulski@gmail.com>
2019-01-16 14:36:03 -0700

    create src/index.js and add semantic stylesheet

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return <div>Hello and welcome!</div>
+};
+
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+);
+ 
