f71a678abbeb421ce7c371838fcb65dbb4b979ac
Malind Kowalski <malind.nikulski@gmail.com>
2019-01-16 14:48:41 -0700

    stub out the jsx for comment

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+//naive approach
 const App = () => {
-  return <div>Hello and welcome!</div>
+  return (
+    <div className='ui container comments'>
+      <div className='comment'>
+        <a href='/' className='avatar'>
+          <img alt='avatar' />
+        </a>
+        <div className='content'>
+          <a href='/' className='author'>
+            Malind
+          </a>
+          <div className='metadata'>
+            <span className='date'>Today at 2:45pm</span>
+          </div>
+          <div className='text'>Nice blog article</div>
+        </div>
+      </div>
+    </div>
+  )
 };
 
 ReactDOM.render(
   <App />,
   document.querySelector('#root')
 );
- 
