969a596a1b47af4d77dfd054ceafb9894122c787
Malind Kowalski <malind.nikulski@gmail.com>
2019-01-16 14:58:58 -0700

    creating a mass naive App

--- src/index.js
+++ src/index.js
@@ -20,6 +20,36 @@ const App = () => {
           <div className='text'>Nice blog article</div>
         </div>
       </div>
+
+      <div className='comment'>
+        <a href='/' className='avatar'>
+          <img alt='avatar' src={faker.image.avatar()}/>
+        </a>
+        <div className='content'>
+          <a href='/' className='author'>
+            Malind
+          </a>
+          <div className='metadata'>
+            <span className='date'>Today at 2:45pm</span>
+          </div>
+          <div className='text'>Nice blog article</div>
+        </div>
+      </div>
+
+      <div className='comment'>
+        <a href='/' className='avatar'>
+          <img alt='avatar' src={faker.image.avatar()}/>
+        </a>
+        <div className='content'>
+          <a href='/' className='author'>
+            Malind
+          </a>
+          <div className='metadata'>
+            <span className='date'>Today at 2:45pm</span>
+          </div>
+          <div className='text'>Nice blog article</div>
+        </div>
+      </div>
     </div>
   )
 };
