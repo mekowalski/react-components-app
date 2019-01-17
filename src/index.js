bd06132789543132ac5c2def70efcb4c8337f14d
Malind Kowalski <malind.nikulski@gmail.com>
2019-01-17 14:29:32 -0700

    wrap remanining instances

--- src/index.js
+++ src/index.js
@@ -15,19 +15,24 @@ const App = () => {
           avatar={faker.image.avatar()}
         />
       </WrappedCard>
-      
-      <CommentDetail
-        author='Ryan'
-        timeAgo='Today at 4:00AM'
-        commentText='I am learning so much React'
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author='Kristopher'
-        timeAgo='Yesterday at 11:15PM'
-        commentText='What the heck is React?'
-        avatar={faker.image.avatar()}
-      />
+
+      <WrappedCard>
+        <CommentDetail
+          author='Ryan'
+          timeAgo='Today at 4:00AM'
+          commentText='I am learning so much React'
+          avatar={faker.image.avatar()}
+        />
+      </WrappedCard>
+
+      <WrappedCard>
+        <CommentDetail
+          author='Kristopher'
+          timeAgo='Yesterday at 11:15PM'
+          commentText='What the heck is React?'
+          avatar={faker.image.avatar()}
+        />
+      </WrappedCard>
     </div>
   );
 };
